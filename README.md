dacc6497b8d16426856779c65673181c990f3240
Alexander Nissen <alexander@feldbech.dk>
2022-08-26 00:11:48 +0200

    Add mouse decoder with animation options

--- README.md
+++ README.md
@@ -37,7 +37,48 @@ Key codes taken from https://gist.github.com/MightyPork/6da26e382a7ad91b5496ee55
 
 ## Mouse
 
-TODO
+Visualizes mouse movement and/or button presses.
+By default draws all movement to a canvas but moves and clicks can optionally be animated, which is useful for stepping through e.g. screen keyboard presses.
+
+```
+usage: mouse_decode.py [-h] [-a] [-d s] [-c] [-m 0-2] file
+
+Decode USB Mouse HID data
+
+positional arguments:
+  file                mouse data file
+
+options:
+  -h, --help          show this help message and exit
+  -a, --animate       animate mouse movement
+  -d s, --delay s     animation delay in seconds after each click (default: 2)
+  -c, --clicks        show mouse clicks explicitly
+  -m 0-2, --mode 0-2  display mode for mouse movement, from less to more verbose [0-2] (default: 1)
+                        0: don't show mouse movement (use with -c for clicks only)
+                        1: show mouse movement when any mouse button is held
+                        2: show all mouse movement
+```
+
+Colors indicate the mouse button(s) pressed when clicking/dragging. Gray is used for movement with no buttons held.
+
+**Examples**
+
+Display drawing made with mouse:
+
+```
+python mouse_decode.py <mousedata-file>
+```
+
+Visualize screen keyboard usage:
+
+```
+python mouse_decode.py <mousedata-file> --mode 0 --clicks --animate
+```
+(optionally mode 2 can be used to see movements in between clicks).
+
+
+
+
 
 ## Tablet
 
---
+++ mouse_decode.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+import argparse
+import struct
+import matplotlib.pyplot as plt
+import signal
+
+
+def to_signed_int(n, bitlength):
+    if n >= 2 ** (bitlength - 1):
+        n -= 2 ** bitlength
+    return n
+
+
+def decode_line(line, format, offset=0):
+    if format == 'short':
+        return struct.unpack('<Bbb', bytes.fromhex(line[:6]))
+    
+    if format == 'long':
+        return (
+            struct.unpack('<B', bytes.fromhex(line[2:4]))[0],
+            to_signed_int(int(line[offset + 3] + line[offset:offset + 2], 16), bitlength=12),
+            to_signed_int(int(line[offset + 4:offset + 6] + line[offset + 2], 16), bitlength=12)
+        )
+
+    raise ValueError(f"'{format}' is not a supported format")
+
+
+def decode(raw_data):
+    clicks, xs, ys = [], [], []
+    x, y = 0, 0
+
+    # Lines can have a few different formats
+    format = ('short', 'long')[len(raw_data[0]) > 8]  # made up names, just to differentiate
+    
+    # x-displacement data can start at offset 4 or 6 for format 'long'
+    # Heuristically guess by checking number of non-zero bytes at the possible boundaries (offset 4-5 and 10-11)
+    offset = 0
+    if format == 'long':
+        evidence_4, evidence_6 = 0, 0
+        for line in raw_data:
+            if line[4:6] != '00':
+                evidence_4 += 1
+            if line[10:12] != '00':
+                evidence_6 += 1
+        offset = (4, 6)[evidence_4 < evidence_6] 
+
+    for line in raw_data:
+        click, x_displacement, y_displacement = decode_line(line, format=format, offset=offset)
+        clicks.append(click)
+        
+        x += x_displacement
+        xs.append(x)
+        
+        y -= y_displacement
+        ys.append(y)
+
+    return clicks, xs, ys
+
+
+def draw(clicks, xs, ys, draw_moves=True, mousedown_only=True, draw_clicks=False, animate=False, click_delay=2):
+    cur_x, cur_y = xs[0], ys[0]
+    mousedown = False
+    
+    plt.axis((min(xs) - 50, max(xs) + 50, min(ys) - 50, max(ys) + 50))
+
+    for click, x, y in zip(clicks, xs, ys):
+        left, right, middle = click & 0b1, (click & 0b10) >> 1, (click & 0b100) >> 2
+        color = (left * 0.8, middle * 0.8, right * 0.8) if click else 'lightgray'
+        
+        if draw_clicks and click and not mousedown:
+            plt.plot(x, y, '+', color=color, markersize=8, zorder=1)
+
+        if draw_moves:
+            if click or not mousedown_only:
+                plt.plot((cur_x, x), (cur_y, y), '-', color=color, linewidth=2 if click else 1, zorder=0)
+        
+        if animate:
+            # Use provided delay on new clicks
+            if draw_clicks and click and not mousedown:
+                plt.pause(click_delay)
+            
+            # Use small delay for any other drawn mouse movement as a crude animation
+            elif draw_moves and (click or not mousedown_only):
+                plt.pause(0.01)
+        
+        cur_x, cur_y = x, y
+        mousedown = click
+
+    plt.show()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Decode USB Mouse HID data', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('file', type=argparse.FileType('r'), help='mouse data file')
+    parser.add_argument('-a', '--animate', action='store_true', help='animate mouse movement')
+    parser.add_argument('-d', '--delay', type=int, default=2, metavar='s', help='animation delay in seconds after each click (default: %(default)s)')
+    parser.add_argument('-c', '--clicks', action='store_true', help='show mouse clicks explicitly')
+    parser.add_argument('-m', '--mode', type=int, choices=range(3), default=1, metavar='0-2', help='''display mode for mouse movement, from less to more verbose [0-2] (default: %(default)s)
+\t0: hide mouse movements (use with -c for clicks only)
+\t1: show mouse movements while any mouse button is held
+\t2: show all mouse movements''')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    mouse_data = [''.join(d.strip().split(':')) for d in args.file.read().split('\n') if d]
+    clicks, xs, ys = decode(mouse_data)
+    
+    signal.signal(signal.SIGINT, lambda signum, frame: exit())  # Graceful close on Ctrl+C during drawing
+    
+    draw(
+        clicks, xs, ys,
+        draw_moves=args.mode > 0,
+        mousedown_only=args.mode == 1,
+        draw_clicks=args.clicks,
+        animate=args.animate,
+        click_delay=args.delay
+    )
+
+
+if __name__ == '__main__':
+    main()
